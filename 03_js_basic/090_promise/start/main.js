b00b88a785c5bf913d137e3c97bcc1389ad743f8
nagi <zwolft9@gmail.com>
2022-10-27 07:32:28 +0900

    complete 19

---
+++ 03_js_basic/090_promise/start/main.js
@@ -0,0 +1,19 @@
+let a = 0;
+console.log(a);
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    a = 1;
+    resolve(a);
+  }, 2000);
+})
+  .then((b) => {
+    console.log(b);
+    return b;
+  })
+  .then((b) => {
+    console.log(b);
+  })
+  .catch((c) => {
+    console.log("catch実行", c);
+  });
