62a809297077dcba5e4b994a61874733399cddda
nagi <zwolft9@gmail.com>
2022-10-31 23:08:22 +0900

    complete 28

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,19 @@
 import "./Child.css";
-
+import { Fragment } from "react";
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <Fragment key="">
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        suscipit ratione rem saepe! Iusto quo sint unde nesciunt natus
+        praesentium hic modi? Provident dolores blanditiis voluptates odio
+        minima, a asperiores!
+      </p>
+    </Fragment>
   );
 };
 
